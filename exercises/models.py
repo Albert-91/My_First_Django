dd3b2ca97a39bee1d392d289ebf71e691cc22978
Albert Suraliński <albert.suralinski@gmail.com>
2018-06-04 22:44:27 +0200

    homework started

--- exercises/models.py
+++ exercises/models.py
@@ -62,3 +62,6 @@ class Song(models.Model):
     title = models.CharField(max_length=128)
     duration = models.TimeField(null=True)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+
+
--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    director = models.ForeignKey(Person, on_delete=models.CASCADE)
+    screenplay = models.ForeignKey(Person, on_delete=models.CASCADE)
+    starring = models.ManyToManyField(Person)
