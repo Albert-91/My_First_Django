dcfdcba9ddb21a9f3f7385fa949a0c18f6dcc3ca
Albert Suraliński <albert.suralinski@gmail.com>
2018-06-05 22:58:51 +0200

    almost done first day homework

--- homework/models.py
+++ homework/models.py
@@ -10,8 +10,15 @@ class Genre(models.Model):
     name = models.CharField(max_length=32)
 
 
+class PersonMovie(models.Model):
+    role = models.CharField(max_length=128)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=128)
     director = models.ForeignKey(Person, on_delete=models.CASCADE)
     screenplay = models.ForeignKey(Person, on_delete=models.CASCADE)
-    starring = models.ManyToManyField(Person)
+    starring = models.ManyToManyField(Person, through=PersonMovie)
+    year = models.IntegerField()
+    rating = models.FloatField()
+    genre = models.ManyToManyField(Genre)
